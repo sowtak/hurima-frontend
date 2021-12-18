734371b3bfbcb95514f954bad2f6540153753481
tkyngs <zovaakasowa@gmail.com>
2021-12-18 17:42:31 +0900

    Init

---
+++ src/main/java/com/tkyngs/hurima/HUrimaApplication.java
@@ -0,0 +1,13 @@
+package com.tkyngs.hurima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HUrimaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HUrimaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tkyngs/hurima/HUrimaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkyngs.hurima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HUrimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
